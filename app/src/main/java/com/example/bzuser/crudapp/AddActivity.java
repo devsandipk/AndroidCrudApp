fc957e53b11ab4558d7259c8facc74d2220a5e1d
sandip kotadiya <dev.sandipk@gmail.com>
2017-07-11 18:34:03 +0530

    changes

--- app/src/main/java/com/example/bzuser/crudapp/AddActivity.java
+++ app/src/main/java/com/example/bzuser/crudapp/AddActivity.java
@@ -1,53 +1,137 @@
 package com.example.bzuser.crudapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.animation.Interpolator;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.List;
 
 public class AddActivity extends AppCompatActivity {
 
-    Button btnSave,btnCancel;
+    Button btnSave, btnCancel, btnUpdate, btnDelete;
+    TextView tvSid;
     EditText etFirstName, etEmail;
     StudentAdapter db = new StudentAdapter(this);
+    ObjectStudent student;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
+        String SID = getIntent().getStringExtra("SID");
+
+
+        tvSid = (TextView) findViewById(R.id.hidden_value);
+        etFirstName = (EditText) findViewById(R.id.etFirstName);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        btnSave = (Button) findViewById(R.id.btnSave);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+        btnUpdate = (Button) findViewById(R.id.btnUpdate);
+        btnDelete = (Button) findViewById(R.id.btnDelete);
+
+        tvSid.setText(SID);
+
+        if (!SID.equals("0")) {
+            student = db.getStudents(Integer.parseInt(SID));
+            etFirstName.setText(student.getFirstname());
+            etEmail.setText(student.getEmail());
+
+            btnSave.setVisibility(View.GONE);
+        } else {
+            btnUpdate.setVisibility(View.GONE);
+            btnDelete.setVisibility(View.GONE);
+        }
+
         /**
          * Insert Student Record
          */
-        btnSave = (Button) findViewById(R.id.btnSave);
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                etFirstName = (EditText) findViewById(R.id.etFirstName);
-                etEmail = (EditText) findViewById(R.id.etEmail);
-                db.addStudent(new ObjectStudent(etFirstName.getText().toString(), etEmail.getText().toString()));
-                Toast.makeText(getApplicationContext(), "Record Successfully Inserted.", Toast.LENGTH_SHORT).show();
-
-                Intent intent=new Intent(AddActivity.this,MainActivity.class);
-                startActivity(intent);
+                if(checkValidation()){
+                    db.addStudent(new ObjectStudent(etFirstName.getText().toString(), etEmail.getText().toString()));
+                    Toast.makeText(getApplicationContext(), "Record Successfully Inserted.", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(AddActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
             }
         });
 
-        btnCancel=(Button) findViewById(R.id.btnCancel);
-        btnCancel.setOnClickListener(new View.OnClickListener(){
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(AddActivity.this,MainActivity.class);
+                if(checkValidation()){
+                    student.setFirstname(etFirstName.getText().toString());
+                    student.setEmail(etEmail.getText().toString());
+                    db.updateStudent(student);
+                        Toast.makeText(getApplicationContext(), "Record Successfully Updated.", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(AddActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder.setCancelable(true);
+                builder.setTitle("Delete?");
+                builder.setMessage("Are you sure want to delete " + student.getFirstname() + " ?");
+                builder.setPositiveButton("Confirm",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                db.deleteStudent(student);
+                                Toast.makeText(getApplicationContext(), "Record Successfully Deleted.", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(AddActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            }
+                        });
+                builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
+
+    }
+
+    public boolean checkValidation() {
+
+        etFirstName = (EditText) findViewById(R.id.etFirstName);
+        if (etFirstName.getText().toString().equals("")) {
+            etFirstName.setError("Please enter value for firstname");
+            return false;
+        }
+
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        if (etEmail.getText().toString().equals("")) {
+            etEmail.setError("Please enter value for email");
+            return false;
+        }
+
+        return true;
     }
 }
--- app/src/main/java/com/example/bzuser/crudapp/MainActivity.java
+++ app/src/main/java/com/example/bzuser/crudapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -27,12 +28,14 @@ public class MainActivity extends AppCompatActivity {
 
         // The desired columns to be bound
         String[] columns = new String[]{
+                db.KEY_ROWID,
                 db.KEY_FIRSTNAME,
                 db.KEY_EMAIL
         };
 
 // the XML defined views which the data will be bound to
         int[] to = new int[]{
+                R.id.tvId,
                 R.id.tvFirstName,
                 R.id.tvEmail
         };
@@ -45,12 +48,24 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(dataAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Cursor cur = (Cursor) parent.getItemAtPosition(position);
+
+                Intent intent=new Intent(MainActivity.this,AddActivity.class);
+                intent.putExtra("SID", String.valueOf(cur.getInt(cur.getColumnIndex(db.KEY_ROWID))));
+                startActivity(intent);
+            }
+        });
+
 
         btnAddNew = (Button) findViewById(R.id.btnAddNew);
         btnAddNew.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddActivity.class);
+                intent.putExtra("SID", "0");
                 startActivity(intent);
             }
         });
